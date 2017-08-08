689c10c95e9e5155bda1fe68ed33772b9d0876a5
willclarktech <willclarktech@users.noreply.github.com>
2017-08-08 17:50:35 +0200

    Update dist-node

--- dist-node/api/privateApi.js
+++ dist-node/api/privateApi.js
@@ -1,7 +1,5 @@
 'use strict';
 
-var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
-
 var _popsicle = require('popsicle');
 
 var popsicle = _interopRequireWildcard(_popsicle);
@@ -173,11 +171,8 @@ function checkReDial() {
  */
 
 function checkOptions(options) {
-	Object.entries(options).forEach(function (_ref) {
-		var _ref2 = _slicedToArray(_ref, 2),
-		    key = _ref2[0],
-		    value = _ref2[1];
-
+	Object.keys(options).forEach(function (key) {
+		var value = options[key];
 		if (value === undefined || Number.isNaN(value)) {
 			throw new Error('parameter value "' + key + '" should not be ' + value);
 		}
--- dist-node/api/utils.js
+++ dist-node/api/utils.js
@@ -1,7 +1,5 @@
 'use strict';
 
-var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
-
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
 function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
@@ -25,13 +23,9 @@ module.exports = {
 			return typeof value === 'string' ? value.trim() : trimObj(value);
 		};
 
-		return isArray ? obj.map(trim) : Object.entries(obj).reduce(function (accumulator, _ref) {
-			var _ref2 = _slicedToArray(_ref, 2),
-			    key = _ref2[0],
-			    value = _ref2[1];
-
+		return isArray ? obj.map(trim) : Object.keys(obj).reduce(function (accumulator, key) {
 			var trimmedKey = trim(key);
-			var trimmedValue = trim(value);
+			var trimmedValue = trim(obj[key]);
 			return Object.assign({}, accumulator, _defineProperty({}, trimmedKey, trimmedValue));
 		}, {});
 	},
@@ -42,12 +36,8 @@ module.exports = {
   * @return query string
   */
 	toQueryString: function toQueryString(obj) {
-		var parts = Object.entries(obj).reduce(function (accumulator, _ref3) {
-			var _ref4 = _slicedToArray(_ref3, 2),
-			    key = _ref4[0],
-			    value = _ref4[1];
-
-			return [].concat(_toConsumableArray(accumulator), [encodeURIComponent(key) + '=' + encodeURI(value)]);
+		var parts = Object.keys(obj).reduce(function (accumulator, key) {
+			return [].concat(_toConsumableArray(accumulator), [encodeURIComponent(key) + '=' + encodeURI(obj[key])]);
 		}, []);
 
 		return parts.join('&');
